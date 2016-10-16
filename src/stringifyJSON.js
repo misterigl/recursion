35a1f195a632c7230853315af70c2ec6c2b17f51
Michael Ilg <michael@ilg.ac>
2016-10-16 15:31:36 -0700

    almost solved stringifyJSON.js, problems with undefined

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,58 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log("obj=", obj);
+  if (obj === null) {
+  	return 'null'
+  }
+
+  if (typeof obj === 'number') {
+  	return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+
+  if (obj === true) {
+  	return 'true';
+  }
+
+  if (obj === false) {
+  	return 'false';
+  }
+
+  if (Array.isArray(obj)) {
+  	return stringifyArray(obj);
+  }
+
+  if (typeof obj ==='object') {
+  	return stringifyObject(obj);
+  }
+
+  function stringifyArray (array) {
+  	console.log("stringifyArray called.");
+  	if (array.length === 0) {
+  		return '[]';
+  	}
+  	var string = '[';
+  	for (var i = 0; i < array.length; i++) {
+  		string = string + stringifyJSON(array[i]) + ',';
+  	}
+
+  	return string.slice(0, string.length-1) + ']';
+  }  	
+
+  function stringifyObject(object) {
+  	console.log("stringifyObject called.");
+  	if (Object.keys(object).length === 0) {
+  		return '{}';
+  	}
+  	var string = '{';
+  	for (key in object) {
+  		string = string + '"' + key + '":' + stringifyJSON(object[key]) + ',';
+  	}
+  	return string.slice(0, string.length-1) + '}';
+  }
+
 };
